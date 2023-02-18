91296f9179dbac27005ee8b8976138f2b0bb0194
nandanjp <nandan.jp17@gmail.com>
2023-02-18 15:17:07 -0500

    Trying to fix profile picture...1

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ const Home = () => (
               borderRadius="full"
               width="100%"
               height="100%"
-              objectFit="contain"
+              layout="fill"
             />
           </Box>
         </Box>
