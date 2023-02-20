a4de3dba2cc50e34540f4529a6610690eb733d5a
nandanjp <nandan.jp17@gmail.com>
2023-02-20 10:23:09 -0500

    Fixed the look of the profile picture.... Including the dumb rotation lol

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ const Home = () => (
               width="100%"
               height="100%"
               objectFit="cover"
-              objectPosition="top -10px left 0"
+              objectPosition="left"
             />
           </Box>
         </Box>
