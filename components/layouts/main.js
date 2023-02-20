037963e2c0976a94a25552d882f05d40906265d1
nandanjp <nandan.jp17@gmail.com>
2023-02-20 12:06:40 -0500

    Removed some remnants

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,19 +15,16 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
         <meta name="author" content="Nandan Patel" />
         <meta name="author" content="nandanjp" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Nandan Patel" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@nandanjp" />
         <meta property="og:site_name" content="Nandan Patel" />
         <meta name="og:title" content="Nandan Patel" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Nandan Patel - Homepage</title>
       </Head>
 
